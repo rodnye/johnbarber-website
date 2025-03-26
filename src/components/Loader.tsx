6c163a2a164d2102f998df3af29190b409fc9002
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-03-26 02:03:35 -0400

    refactor: add image loader hook in Loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,34 +1,20 @@
-import { useEffect, useState } from 'react';
-import imageSrc from '../assets/icon_logo.svg';
-import styled from 'styled-components';
+import { useImageLoader } from "../utils/loader";
+import styled from "styled-components";
+import imageSrc from "../assets/icon_logo.svg";
 
-export function Loader({ className = '' }) {
-  const [image, setImage] = useState<HTMLImageElement | null>(null);
+export function Loader({ className = "" }) {
+  const { isLoaded } = useImageLoader([imageSrc]);
 
-  useEffect(() => {
-    const image = new Image();
-    image.src = imageSrc;
-    image.addEventListener("load", () => {
-      setImage(image);
-    });
-  }, []);
-
-  if (!image) return (
-    <div> wiii </div>
-  )
+  if (!isLoaded) return <div> loading... </div>;
 
   return (
     <LoaderContainer className={className}>
-      <img 
-        className='image'
-        src={image.src} 
-        alt='Logo Image' 
-      />
-      <div className='loader'>
-        <div className='block'/>
-        <div className='block'/>
-        <div className='block'/>
-        <div className='block'/>
+      <img className="image" src={imageSrc} alt="JohnBarber Logo" />
+      <div className="loader">
+        <div className="block" />
+        <div className="block" />
+        <div className="block" />
+        <div className="block" />
       </div>
     </LoaderContainer>
   );
@@ -45,12 +31,12 @@ const LoaderContainer = styled.div`
     width: 5em;
     margin: 0em auto;
   }
-  
+
   .image {
     width: 5em;
     animation: faded 5s ease-in-out alternate infinite;
   }
-  
+
   .block {
     position: relative;
     box-sizing: border-box;
@@ -61,41 +47,41 @@ const LoaderContainer = styled.div`
     border-radius: 0.1875em;
     background: black;
   }
-  
-  .block:nth-child(4n+1) {
-    animation: wave_23 2s ease .0s infinite;
+
+  .block:nth-child(4n + 1) {
+    animation: wave_23 2s ease 0s infinite;
   }
-  
-  .block:nth-child(4n+2) {
-    animation: wave_23 2s ease .2s infinite;
+
+  .block:nth-child(4n + 2) {
+    animation: wave_23 2s ease 0.2s infinite;
   }
-  
-  .block:nth-child(4n+3) {
-    animation: wave_23 2s ease .4s infinite;
+
+  .block:nth-child(4n + 3) {
+    animation: wave_23 2s ease 0.4s infinite;
   }
-  
-  .block:nth-child(4n+4) {
-    animation: wave_23 2s ease .6s infinite;
+
+  .block:nth-child(4n + 4) {
+    animation: wave_23 2s ease 0.6s infinite;
     margin-right: 0;
   }
-  
+
   @keyframes wave_23 {
     0% {
       top: 0;
       opacity: 1;
     }
-  
+
     50% {
       top: 1.875em;
-      opacity: .2;
+      opacity: 0.2;
     }
-  
+
     100% {
       top: 0;
       opacity: 1;
     }
   }
-  
+
   @keyframes faded {
     0% {
       opacity: 30%;
@@ -104,4 +90,4 @@ const LoaderContainer = styled.div`
       opacity: 80%;
     }
   }
-`
+`;
